f0d713137ff48cbc95c1493ca7b79b2579e78cc9
sreedhar111yu <sreedharv36@gmail.com>
2024-01-20 23:10:44 +0530

    add routes and pages

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Profile from './Pages/Profile';
+import Signin from './Pages/Signin';
+import Signup from './Pages/Signup';
+
 
 function App() {
   return (
-   <>App</>
+   <>
+   <BrowserRouter>
+   <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/about' element={<About/>}/>
+     <Route path='/signin' element={<Signin/>}/>
+     <Route path='/signup' element={<Signup/>}/>
+     <Route path='/profile' element={<Profile/>}/>
+   </Routes>
+   
+   </BrowserRouter>
+   
+   
+   </>
   )
 }
 
---
+++ vite-project/src/Pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <>Signup</>
+  )
+}
+
+export default Signup
