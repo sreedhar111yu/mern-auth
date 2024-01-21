c0f5037ba52deaac93775a7974049c7aec5968db
sreedhar111yu <sreedharv36@gmail.com>
2024-01-21 11:37:30 +0530

    connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
+// server.js
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app =express();
+dotenv.config();
 
+console.log('MongoDB URI:', process.env.MONGODB_URI); // Log the MongoDB URI
 
-app.listen(3000,()=>{
-    console.log("server running prot 3000!");
-})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000!");
+});
