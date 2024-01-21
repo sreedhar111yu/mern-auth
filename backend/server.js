bf9263a478f4af975c9d84ffdd1988cb9475386d
sreedhar111yu <sreedharv36@gmail.com>
2024-01-21 12:42:14 +0530

    create api route and controllers

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -20,3 +21,6 @@ const app = express();
 app.listen(3000, () => {
     console.log("Server running on port 3000!");
 });
+
+
+app.use("/api/user",userRoutes);
