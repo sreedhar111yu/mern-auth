f4690ec9447e2aa539697ad6d420774541aa21ae
sreedhar111yu <sreedharv36@gmail.com>
2024-01-22 09:22:43 +0530

    added jsonwebtoken

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from'../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import {errorHandler} from '../utils/error.js'
+import jwt from 'jsonwebtoken';
 
 export const signup =async(req, res,next) => {
     const {username, email, password }=req.body;
@@ -14,4 +16,19 @@ export const signup =async(req, res,next) => {
 
     }
      
+};
+export const signin =async(req, res, next)=>{
+    const {email,password}=req.body;
+    try {
+        const validUser =await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,'User not found'));
+        //check pw valid
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,'Invalid password'));
+        const token =jwt.sign({id:validPassword._id }, process.env.JWT_SECRET)
+        res.cookie('acces_token',token, {httpOnly:true}).status(200).json(validUser)
+    } catch (error) {
+        next(error);
+        
+    }
 }
