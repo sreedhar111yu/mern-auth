4db98f4e499c630078efb5012544f1783df2e9c1
sreedhar111yu <sreedharv36@gmail.com>
2024-01-22 21:08:10 +0530

    add redux persist

--- vite-project/src/redux/store.js
+++ vite-project/src/redux/store.js
@@ -1,11 +1,24 @@
-import {configureStore} from '@reduxjs/toolkit';
-import  useReducer  from './user/userSlice';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const rootReducer = combineReducers({ user: userReducer });
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducerInstance = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer:{user:useReducer},
-   middleware:(getDefaultMiddleware) => getDefaultMiddleware({
-    serializableCheck:false,
-   }),
+  reducer: persistedReducerInstance,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
