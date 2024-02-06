f927a5384fce3449b538fae41a9695c7b774c829
sreedhar111yu <sreedharv36@gmail.com>
2024-02-06 11:43:26 +0530

    06-02

--- vite-project/src/Components/Navbar.jsx
+++ vite-project/src/Components/Navbar.jsx
@@ -1,34 +1,76 @@
 import React from 'react'
+import { useState } from 'react';
 import{ Link } from 'react-router-dom';
 import {useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars,faXmark } from '@fortawesome/free-solid-svg-icons'
+ 
 
 
 function Navbar() {
+  const[toggle,setToggle]=useState(false)
+  const handleclick =()=> setToggle(!toggle)
+
+
+
   const {currentUser}=useSelector((state)=> state.user);
 
   return (
    <>
-   <div className='bg-yellow-400'>
-    <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+   <div className='w-full h-[80px] bg-white '>
+    <div className='max-w-[1280px]  m-auto w-full h-full flex justify-between items-center'>
         <Link to='/'>
-        <h1 className='font-bold'> Auth app</h1>
+        <h1 className='font-bold text-3xl font-serif  text-yellow-500'> Auth app</h1>
         </Link>
-        <ul className='flex gap-4'>
+        <ul className='hidden md:flex gap-8 font-bold'>
             <Link className='hover:text-yellow-400' to='/'><li>Home</li></Link>
             <Link className='hover:text-yellow-400' to='/about'><li>About</li></Link>
             <Link className='hover:text-yellow-400' to='/menu'><li>Menu</li></Link>
+          </ul>
+          
 
+            <div className='hidden md:flex  '>
             <Link className='hover:text-yellow-400' to='/profile'>
             {currentUser ? (
               <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
             ) : (
-              <li>Sign In</li>
+              <li className='px-8 py-3 rounded font-bold text-white bg-yellow-500'>Sign In</li>
             )}
           </Link>
+            </div>
+            <div className='md:hidden mr-5 flex text-2xl float-end' onClick={handleclick}>
+            <FontAwesomeIcon icon={toggle?open:faBars} />
+            <div className={toggle?close:'hidden'}>
+            <FontAwesomeIcon icon={faXmark} />
+
+            </div>
             
-        </ul>
+
+            </div>
+
+            
+            <div >
+            <ul className={toggle?'absolute z-10 p-4 bg-white space-y-4 w-full font-bold md:hidden':'hidden'}>
+            <li className='hover:text-yellow-500 p-4 hover:bg-gray-100'> <Link to='/'>Home</Link></li>
+            <li className='hover:text-yellow-500 p-4 hover:bg-gray-100'><Link to='/about'>About</Link></li>
+            <li className='hover:text-yellow-500 p-4 hover:bg-gray-100'><Link to='/menu'>Menu</Link></li>
+            <li className='hover:text-yellow-500 p-4 hover:bg-gray-100'><Link to='services'>Services</Link></li>
+
+            <div className=''>
+                
+                <button className='px-8 py-3 rounded font-bold text-white bg-yellow-500 '><Link to='/signin'> Sign In</Link></button>
+            </div >
+                
+            </ul>
+           
+           </div>
+          
+            
+        
     </div>
    </div>
+
+   
    
    </>
   )
--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
-function Home() {
+import img from '../image/bg.png'
+const Home = () => {
   return (
-    <div>Home</div>
+    <>
+  <div className='w-full  flex '>
+    <div className="lg:w-[50vw]  h-[100vh] py-5 lg:py-36  ml-10">
+      <div className='font-serif font-bold px-12   p-8 py-2 ml-12'>
+        <h1 className='text-6xl py-4' >Taste the <span className='text-yellow-500 '>Sweet<br/></span>Side of Life</h1>
+      <p className='font-bold py-4'>We provide you the tasty and  delicious<br /> homemade dessert</p>
+    <button className='bg-yellow-500 rounded px-6  py-3 ml-12 mt-12 font-bold  text-white '>Explore</button>
+    </div>
+    </div>
+    <div className="  w-[50vw] mb:hidden h-[100vh]">
+    <img className='   max-w-[550px] float-right 'src={img} alt="" /> 
+
+    </div>
+   
+   
+   </div>
+
+   
+   
+   
+   
+
+  
+
+    </>
   )
 }
 
